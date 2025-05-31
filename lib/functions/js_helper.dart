7017cc8405af2820bd0f336f0cf197dcc7401211
islomkhodja <islomkhodjadev@gmail.com>
2025-05-31 15:56:01 +0500

    added telegram js

--- lib/functions/js_helper.dart
+++ lib/functions/js_helper.dart
@@ -1,45 +1,47 @@
 import 'package:boyshub/telegram_js.dart' as tg_js;
-
 import 'dart:convert';
 
 String? getTelegramChatId() {
   try {
-    final user = tg_js.webApp.initDataUnsafe.user;
-    if (user != null && user.id != null) {
+    final user = tg_js.webApp?.initDataUnsafe?.user;
+    if (user != null) {
       return user.id.toString();
     }
   } catch (e) {
-    print('Telegram WebApp not found: $e');
+    print('Telegram WebApp not found or user.id not accessible: $e');
   }
   return null;
 }
 
-
 String? getTelegramAllData() {
   try {
     final webApp = tg_js.webApp;
-    final user = webApp.initDataUnsafe.user;
+    if (webApp == null) {
+      print("webApp is null");
+      return null;
+    }
 
-    // Create a Map with everything you want to show
+    final user = webApp.initDataUnsafe.user;
+    final theme = webApp.themeParams;
+
     final map = {
-      'id': user?.id,
-      'username': user?.username,
-      'first_name': user?.first_name,
-      'last_name': user?.last_name,
+      'id': user.id,
+      'username': user.username,
+      'first_name': user.first_name,
+      'last_name': user.last_name,
       'themeParams': {
-        'bg_color': webApp.themeParams.bg_color,
-        'text_color': webApp.themeParams.text_color,
-        'hint_color': webApp.themeParams.hint_color,
-        'link_color': webApp.themeParams.link_color,
-        'button_color': webApp.themeParams.button_color,
-        'button_text_color': webApp.themeParams.button_text_color,
+        'bg_color': theme.bg_color,
+        'text_color': theme.text_color,
+        'hint_color': theme.hint_color,
+        'link_color': theme.link_color,
+        'button_color': theme.button_color,
+        'button_text_color': theme.button_text_color,
       },
-      // You can add more properties from webApp or initDataUnsafe as needed
     };
 
     return const JsonEncoder.withIndent('  ').convert(map);
   } catch (e) {
-    print('Telegram WebApp not found: $e');
+    print('Error accessing Telegram WebApp data: $e');
     return null;
   }
 }
